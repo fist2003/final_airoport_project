8e5cea507f84dc5164f1baa0f4f6abf0f4862b5b
Shevchenko <fist2003@list.ru>
2016-12-14 19:27:38 +0200

    airport final

---
+++ src/main/java/service/Main.java
@@ -0,0 +1,15 @@
+package service;
+
+import view.StartMenuGUI;
+
+/**
+ * Created by ПК on 08.12.2016.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        StartMenuGUI instStartMenuGUI = new StartMenuGUI();
+        instStartMenuGUI.startMenu();
+    }
+}
